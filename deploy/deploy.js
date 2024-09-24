fecb7b21994d9d05439adda5b20987c4ad6e72bf
Manuel Montenegro <manuel@nucypher.com>
2024-09-24 14:15:53 +0200

    Fix deployment script

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -6,19 +6,12 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
     console.log('network id ', await getChainId());
 
     const {deploy} = deployments;
-    const {deployer, tokenContract, rewardsHolder, tacoApp, owner} = await getNamedAccounts();
-    const args = [tokenContract, tacoApp, rewardsHolder, owner];
+    const {deployer, tokenContract, rewardsHolder, owner} = await getNamedAccounts();
+    const args = [tokenContract, rewardsHolder, owner];
     const merkleDrop = await deploy('CumulativeMerkleDrop', {
         from: deployer,
         args,
     });
 
     console.log('CumulativeMerkleDrop deployed to:', merkleDrop.address);
-
-    if (await getChainId() !== '31337') {
-        await hre.run('verify:verify', {
-            address: merkleDrop.address,
-            constructorArguments: args,
-        });
-    }
 };
